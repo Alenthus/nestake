b18c4b3b867b9e765502fbf73706d3ba5c293d1b
mathetake <cz.rk.t0415y.g@gmail.com>
2018-10-25 23:42:19 +0900

    imple adc, and, asl instructions

--- src/internal/cpu.cpp
+++ src/internal/cpu.cpp
@@ -8,224 +8,271 @@ using std::array;
 using std::map;
 using std::string;
 
+namespace nestake {
 
-// instructions's identifier supposed to be used in debugging
-enum InstructionID {
-    ADC = 1, AHX, ALR, ANC, AND, ARR, ASL, AXS, BCC, BCS, BEQ, BIT, BMI,
-    BNE, BPL, BRK, BVC, BVS, CLC, CLD, CLI, CLV, CMP, CPX, CPY, DCP, DEC,
-    DEX, DEY, EOR, INC, INX, INY, ISC, JMP, JSR, KIL, LAS, LAX, LDA, LDX,
-    LDY, LSR, NOP, ORA, PHA, PHP, PLA, PLP, RLA, ROL, ROR, RRA, RTI, RTS,
-    SAX, SBC, SEC, SED, SEI, SHX, SHY, SLO, SRE, STA, STX, STY, TAS, TAX,
-    TAY, TSX, TXA, TXS, TYA, XAA,
-};
+    // instructions's identifier supposed to be used in debugging
+    enum InstructionID {
+        ADC = 1, AHX, ALR, ANC, AND, ARR, ASL, AXS, BCC, BCS, BEQ, BIT, BMI,
+        BNE, BPL, BRK, BVC, BVS, CLC, CLD, CLI, CLV, CMP, CPX, CPY, DCP, DEC,
+        DEX, DEY, EOR, INC, INX, INY, ISC, JMP, JSR, KIL, LAS, LAX, LDA, LDX,
+        LDY, LSR, NOP, ORA, PHA, PHP, PLA, PLP, RLA, ROL, ROR, RRA, RTI, RTS,
+        SAX, SBC, SEC, SED, SEI, SHX, SHY, SLO, SRE, STA, STX, STY, TAS, TAX,
+        TAY, TSX, TXA, TXS, TYA, XAA,
+    };
 
-map<InstructionID, string> idToInstructionName = {
-    {ADC, "ADC"}, {AHX, "AHX"}, {ALR, "ALR"}, {ANC, "ANC"}, {AND, "AND"}, {ARR, "ARR"}, {ASL, "ASL"}, {AXS, "AXS"}, {BCC, "BCC"}, {BCS, "BCS"}, {BEQ, "BEQ"}, {BIT, "BIT"}, {BMI, "BMI"}, {BNE, "BNE"}, {BPL, "BPL"}, {BRK, "BRK"},
-    {BVC, "BVC"}, {BVS, "BVS"}, {CLC, "CLC"}, {CLD, "CLD"}, {CLI, "CLI"}, {CLV, "CLV"}, {CMP, "CMP"}, {CPX, "CPX"}, {CPY, "CPY"}, {DCP, "DCP"}, {DEC, "DEC"}, {DEX, "DEX"}, {DEY, "DEY"}, {EOR, "EOR"}, {INC, "INC"}, {INX, "INX"},
-    {INY, "INY"}, {ISC, "ISC"}, {JMP, "JMP"}, {JSR, "JSR"}, {KIL, "KIL"}, {LAS, "LAS"}, {LAX, "LAX"}, {LDA, "LDA"}, {LDX, "LDX"}, {LDY, "LDY"}, {LSR, "LSR"}, {NOP, "NOP"}, {ORA, "ORA"}, {PHA, "PHA"}, {PHP, "PHP"}, {PLA, "PLA"},
-    {PLP, "PLP"}, {RLA, "RLA"}, {ROL, "ROL"}, {ROR, "ROR"}, {RRA, "RRA"}, {RTI, "RTI"}, {RTS, "RTS"}, {SAX, "SAX"}, {SBC, "SBC"}, {SEC, "SEC"}, {SED, "SED"}, {SEI, "SEI"}, {SHX, "SHX"}, {SHY, "SHY"}, {SLO, "SLO"}, {SRE, "SRE"},
-    {STA, "STA"}, {STX, "STX"}, {STY, "STY"}, {TAS, "TAS"}, {TAX, "TAX"}, {TAY, "TAY"}, {TSX, "TSX"}, {TXA, "TXA"}, {TXS, "TXS"}, {TYA, "TYA"}, {XAA, "XAA"}
-};
+    map<InstructionID, string> idToInstructionName = {
+        {ADC, "ADC"}, {AHX, "AHX"}, {ALR, "ALR"}, {ANC, "ANC"}, {AND, "AND"}, {ARR, "ARR"}, {ASL, "ASL"}, {AXS, "AXS"}, {BCC, "BCC"}, {BCS, "BCS"}, {BEQ, "BEQ"}, {BIT, "BIT"}, {BMI, "BMI"}, {BNE, "BNE"}, {BPL, "BPL"}, {BRK, "BRK"},
+        {BVC, "BVC"}, {BVS, "BVS"}, {CLC, "CLC"}, {CLD, "CLD"}, {CLI, "CLI"}, {CLV, "CLV"}, {CMP, "CMP"}, {CPX, "CPX"}, {CPY, "CPY"}, {DCP, "DCP"}, {DEC, "DEC"}, {DEX, "DEX"}, {DEY, "DEY"}, {EOR, "EOR"}, {INC, "INC"}, {INX, "INX"},
+        {INY, "INY"}, {ISC, "ISC"}, {JMP, "JMP"}, {JSR, "JSR"}, {KIL, "KIL"}, {LAS, "LAS"}, {LAX, "LAX"}, {LDA, "LDA"}, {LDX, "LDX"}, {LDY, "LDY"}, {LSR, "LSR"}, {NOP, "NOP"}, {ORA, "ORA"}, {PHA, "PHA"}, {PHP, "PHP"}, {PLA, "PLA"},
+        {PLP, "PLP"}, {RLA, "RLA"}, {ROL, "ROL"}, {ROR, "ROR"}, {RRA, "RRA"}, {RTI, "RTI"}, {RTS, "RTS"}, {SAX, "SAX"}, {SBC, "SBC"}, {SEC, "SEC"}, {SED, "SED"}, {SEI, "SEI"}, {SHX, "SHX"}, {SHY, "SHY"}, {SLO, "SLO"}, {SRE, "SRE"},
+        {STA, "STA"}, {STX, "STX"}, {STY, "STY"}, {TAS, "TAS"}, {TAX, "TAX"}, {TAY, "TAY"}, {TSX, "TSX"}, {TXA, "TXA"}, {TXS, "TXS"}, {TYA, "TYA"}, {XAA, "XAA"}
+    };
 
-// addressing mode
-enum AddressingMode {
-    Absolute = 1, AbsoluteX, AbsoluteY, Accumulator, Immediate,
-    Implied, IndexedIndirect, Indirect, IndirectIndexed, Relative,
-    ZeroPage, ZeroPageX, ZeroPageY
-};
+    void _adc(Cpu* cpu, uint16_t address){
+        uint8_t a = cpu->A;
+        uint8_t b = cpu->Memory->Read(address);
+        cpu->A = a + b + cpu->C;
 
-// type of interruption
-enum InterruptType {
-    interruptNone = 1, interruptNMI, interruptIRQ,
-};
+        // set Z & N flags
+        cpu->setZN(cpu->A);
 
-bool isPageDiffer(uint16_t a, uint16_t b) {
-    return (a&0xFF00) == (b&0xFF00);
-}
+        // set carry flag
+        if ((int(a) + int(b) + int(cpu->C)) > 0xFF) {
+            cpu->C = 1;
+        } else {
+            cpu->C = 0;
+        }
 
-uint16_t NesCpu::read16(uint16_t address) {
-    uint16_t lo = Memory->Read(address);
-    uint16_t hi = Memory->Read(address + uint16_t(1)) <<8;
-    return hi | lo;
-}
-
-uint16_t NesCpu::read16Bug(uint16_t address) {
-    uint16_t lo = address;
-    uint16_t hi = (address&uint16_t(0xFF00)) | uint16_t(address+1) <<8;
-    return uint16_t(hi)<<8 | uint16_t(lo);
-}
-
-void NesCpu::push(uint8_t v) {
-    Memory->Write(uint16_t(0x100)|uint16_t(SP), v);
-    --SP;
-}
-
-void NesCpu::push16(uint16_t v) {
-    push(uint8_t(v >> 8));
-    push(uint8_t(v & 0xFF));
-}
-
-void NesCpu::setN(uint8_t v) {
-    if (v != 0) {
-        N = 1;
-    } else {
-        N = 0;
+        // set overflow flag
+        if ((((a^b)&0x80) == 0) && (a^cpu->A != 0)){
+            cpu->V = 1;
+        } else {
+            cpu->V = 0;
+        }
     }
 
-}
-
-void NesCpu::setZ(uint8_t v) {
-    if (v == 0) {
-        Z = 1;
-    } else {
-        Z = 0;
-    }
-}
-
-void NesCpu::setZN(uint8_t v) {
-    setZ(v);
-    setN(v);
-}
-
-void NesCpu::compare(uint8_t a, uint8_t b) {
-    setZN(a-b);
-    if (a >= b) {
-        C = 1;
-    } else {
-        C = 0;
-    }
-}
-
-void NesCpu::setFlags(uint8_t flag) {
-    C = (flag >> 0) & uint8_t(1);
-    Z = (flag >> 1) & uint8_t(1);
-    I = (flag >> 2) & uint8_t(1);
-    D = (flag >> 3) & uint8_t(1);
-    B = (flag >> 4) & uint8_t(1);
-    U = (flag >> 5) & uint8_t(1);
-    V = (flag >> 6) & uint8_t(1);
-    N = (flag >> 7) & uint8_t(1);
-}
-
-uint8_t NesCpu::pull() {
-    ++SP;
-    return Memory->Read(uint16_t(0x100)|uint16_t(SP));
-}
-
-uint16_t NesCpu::pull16() {
-    return uint16_t(pull()) << 8 | uint16_t(pull());
-}
-
-void NesCpu::TriggerIRQ() {
-    if (I == 0) {
-        interrupt = interruptIRQ;
-    }
-}
-
-void NesCpu::TriggerNMI() {
-    interrupt = interruptNMI;
-}
-
-
-void NesCpu::irq() {
-    // TODO: implement
-}
-
-void NesCpu::nmi() {
-    // TODO: implement
-}
-
-// Array of instruction params to be selected by its corresponding bits.
-array<InstructionParams*, 256> InstructionTable = {};
-
-uint64_t NesCpu::Step() {
-
-    // stall cpu cycle
-    if (stall > 0) {
-        --stall;
-        return 1;
+    void _and(Cpu* cpu, uint16_t address, bool is_accumulator){
+        cpu->A = cpu->A & cpu->Memory->Read(address);
+        // set Z & N flags
+        cpu->setZN(cpu->A);
     }
 
-    switch (interrupt) {
-        case interruptNMI:
-            nmi();
-            break;
-        case interruptIRQ:
-            irq();
-            break;
-        default:{}
+    void _asl(Cpu* cpu, uint16_t address, bool is_accumulator){
+        if (is_accumulator) {
+            cpu->C = (cpu->C >> 7) & uint8_t(1);
+            cpu->A <<= 1;
+            cpu->setZN(cpu->A);
+        } else {
+            uint8_t v = cpu->Memory->Read(address);
+            cpu->C = (v >> 7) & uint8_t(1);
+            v <<= 1;
+            cpu->Memory->Write(address, v);
+            cpu->setZN(v);
+        }
     }
-    // reset interrupt flag
-    interrupt = interruptNone;
 
-    // auxiliary variables
-    uint64_t prev_cycles = Cycles;
-    uint16_t address = 0;
-    bool page_crossed = false;
-    InstructionParams* inst = InstructionTable[PC];
+    // addressing mode
+    enum AddressingMode {
+        Absolute = 1, AbsoluteX, AbsoluteY, Accumulator, Immediate,
+        Implied, IndexedIndirect, Indirect, IndirectIndexed, Relative,
+        ZeroPage, ZeroPageX, ZeroPageY
+    };
 
-    // switching the instruction's condition depending on addressing mode
-    switch (inst->AddressingMode) {
-        case Absolute : {
-            address = read16(PC + uint16_t(1));
-            break;
+    // type of interruption
+    enum InterruptType {
+        interruptNone = 1, interruptNMI, interruptIRQ,
+    };
+
+    bool isPageDiffer(uint16_t a, uint16_t b) {
+        return (a&0xFF00) == (b&0xFF00);
+    }
+
+    uint16_t Cpu::read16(uint16_t address) {
+        uint16_t lo = Memory->Read(address);
+        uint16_t hi = Memory->Read(address + uint16_t(1)) <<8;
+        return hi | lo;
+    }
+
+    uint16_t Cpu::read16Bug(uint16_t address) {
+        uint16_t lo = address;
+        uint16_t hi = (address&uint16_t(0xFF00)) | uint16_t(address+1) <<8;
+        return uint16_t(hi)<<8 | uint16_t(lo);
+    }
+
+    void Cpu::push(uint8_t v) {
+        Memory->Write(uint16_t(0x100)|uint16_t(SP), v);
+        --SP;
+    }
+
+    void Cpu::push16(uint16_t v) {
+        push(uint8_t(v >> 8));
+        push(uint8_t(v & 0xFF));
+    }
+
+    void Cpu::setN(uint8_t v) {
+        if (v != 0) {
+            N = 1;
+        } else {
+            N = 0;
         }
-        case AbsoluteX: {
-            address = read16(PC + uint16_t(1)) + uint8_t(X);
-            page_crossed = isPageDiffer(address - uint8_t(X), address);
-            break;
+
+    }
+
+    void Cpu::setZ(uint8_t v) {
+        if (v == 0) {
+            Z = 1;
+        } else {
+            Z = 0;
         }
-        case AbsoluteY: {
-            address = read16(PC + uint16_t(1)) + uint8_t(Y);
-            page_crossed = isPageDiffer(address - uint8_t(Y), address);
-            break;
+    }
+
+    void Cpu::setZN(uint8_t v) {
+        setZ(v);
+        setN(v);
+    }
+
+    void Cpu::compare(uint8_t a, uint8_t b) {
+        setZN(a-b);
+        if (a >= b) {
+            C = 1;
+        } else {
+            C = 0;
         }
-        case Immediate: {
-            address = PC + uint16_t(1);
-            break;
+    }
+
+    void Cpu::setFlags(uint8_t flag) {
+        C = (flag >> 0) & uint8_t(1);
+        Z = (flag >> 1) & uint8_t(1);
+        I = (flag >> 2) & uint8_t(1);
+        D = (flag >> 3) & uint8_t(1);
+        B = (flag >> 4) & uint8_t(1);
+        U = (flag >> 5) & uint8_t(1);
+        V = (flag >> 6) & uint8_t(1);
+        N = (flag >> 7) & uint8_t(1);
+    }
+
+    uint8_t Cpu::pull() {
+        ++SP;
+        return Memory->Read(uint16_t(0x100)|uint16_t(SP));
+    }
+
+    uint16_t Cpu::pull16() {
+        return uint16_t(pull()) << 8 | uint16_t(pull());
+    }
+
+    void Cpu::TriggerIRQ() {
+        if (I == 0) {
+            interrupt = interruptIRQ;
         }
-        case IndexedIndirect: {
-            address = read16Bug(Memory->Read(PC + uint16_t(1)) + uint16_t (X));
-            break;
+    }
+
+    void Cpu::TriggerNMI() {
+        interrupt = interruptNMI;
+    }
+
+
+    void Cpu::irq() {
+        // TODO: implement
+    }
+
+    void Cpu::nmi() {
+        // TODO: implement
+    }
+
+    // Array of instruction params to be selected by its corresponding bits.
+    array<InstructionParams*, 256> InstructionTable = {};
+
+    uint64_t Cpu::Step() {
+
+        // stall cpu cycle
+        if (stall > 0) {
+            --stall;
+            return 1;
         }
-        case Indirect: {
-            address = read16Bug(read16(PC + uint16_t(1)));
-            break;
-        }
-        case IndirectIndexed: {
-            address = read16Bug(Memory->Read(PC + uint16_t(1))) + uint16_t (Y);
-            page_crossed = isPageDiffer(address -uint16_t(Y), address);
-        }
-        case Relative: {
-            uint16_t offset = Memory->Read(PC + uint16_t(1));
-            if (offset < 0x80) {
-                address = PC + uint16_t(2) + offset;
-            } else {
-                address = PC + uint16_t(2) + offset - uint16_t(0x100);
+
+        switch (interrupt) {
+            case interruptNMI:
+                nmi();
+                break;
+            case interruptIRQ:
+                irq();
+                break;
+            default: {
             }
-            break;
         }
-        case ZeroPage: {
-            address = Memory->Read(PC + uint16_t(1));
-            break;
-        }
-        case ZeroPageX: {
-            address = uint16_t((Memory->Read(PC + uint16_t(1)) + X) & 0xff);
-            break;
-        }
-        case ZeroPageY: {
-            address = uint16_t((Memory->Read(PC + uint16_t(1)) + Y) & 0xff);
-            break;
-        }
-        default: {}
-    }
+        // reset interrupt flag
+        interrupt = interruptNone;
 
-    PC += inst->InstructionSizes;
-    Cycles += inst->InstructionCycle;
-    if (page_crossed) {
-        Cycles += inst->PageCycle;
-    }
+        // auxiliary variables
+        uint64_t prev_cycles = Cycles;
+        uint16_t address = 0;
+        bool page_crossed = false;
+        InstructionParams *inst = InstructionTable[PC];
 
-    inst->executor(this, address);
-    return Cycles - prev_cycles;
+        // switch the instruction's condition according to the addressing mode
+        switch (inst->AddressingMode) {
+            case Absolute : {
+                address = read16(PC + uint16_t(1));
+                break;
+            }
+            case AbsoluteX: {
+                address = read16(PC + uint16_t(1)) + uint8_t(X);
+                page_crossed = isPageDiffer(address - uint8_t(X), address);
+                break;
+            }
+            case AbsoluteY: {
+                address = read16(PC + uint16_t(1)) + uint8_t(Y);
+                page_crossed = isPageDiffer(address - uint8_t(Y), address);
+                break;
+            }
+            case Immediate: {
+                address = PC + uint16_t(1);
+                break;
+            }
+            case IndexedIndirect: {
+                address = read16Bug(Memory->Read(PC + uint16_t(1)) + uint16_t(X));
+                break;
+            }
+            case Indirect: {
+                address = read16Bug(read16(PC + uint16_t(1)));
+                break;
+            }
+            case IndirectIndexed: {
+                address = read16Bug(Memory->Read(PC + uint16_t(1))) + uint16_t(Y);
+                page_crossed = isPageDiffer(address - uint16_t(Y), address);
+            }
+            case Relative: {
+                uint16_t offset = Memory->Read(PC + uint16_t(1));
+                if (offset < 0x80) {
+                    address = PC + uint16_t(2) + offset;
+                } else {
+                    address = PC + uint16_t(2) + offset - uint16_t(0x100);
+                }
+                break;
+            }
+            case ZeroPage: {
+                address = Memory->Read(PC + uint16_t(1));
+                break;
+            }
+            case ZeroPageX: {
+                address = uint16_t((Memory->Read(PC + uint16_t(1)) + X) & 0xff);
+                break;
+            }
+            case ZeroPageY: {
+                address = uint16_t((Memory->Read(PC + uint16_t(1)) + Y) & 0xff);
+                break;
+            }
+            default: {
+            }
+        }
+
+        PC += inst->InstructionSizes;
+        Cycles += inst->InstructionCycle;
+        if (page_crossed) {
+            Cycles += inst->PageCycle;
+        }
+
+        inst->executor(this, address, inst->AddressingMode == Accumulator);
+        return Cycles - prev_cycles;
+    }
 }
--- src/internal/cpu.hpp
+++ src/internal/cpu.hpp
@@ -4,109 +4,110 @@
 #include <array>
 #include <map>
 #include "memory.hpp"
+namespace nestake {
+    class Cpu {
+    public:
+        // pointer to NesMemory interface
+        NesMemory* Memory;
 
-class NesCpu {
-public:
-    // pointer to NesMemory interface
-    NesMemory* Memory;
+        // cpu cycles
+        uint64_t Cycles;
 
-    // cpu cycles
-    uint64_t Cycles;
+        // program counter
+        uint16_t PC;
 
-    // program counter
-    uint16_t PC;
+        // stack pointer
+        uint8_t SP;
 
-    // stack pointer
-    uint8_t SP;
+        // accumulator
+        uint8_t A;
 
-    // accumulator
-    uint8_t A;
+        // x register
+        uint8_t X;
 
-    // x register
-    uint8_t X;
+        // y register
+        uint8_t Y;
 
-    // y register
-    uint8_t Y;
+        // c register
+        uint8_t C;
 
-    // c register
-    uint8_t C;
+        // zero flag
+        uint8_t Z;
 
-    // zero flag
-    uint8_t Z;
+        // interrupt disable flag
+        uint8_t I;
 
-    // interrupt disable flag
-    uint8_t I;
+        // decimal mode flag
+        uint8_t D;
 
-    // decimal mode flag
-    uint8_t D;
+        //break command flag
+        uint8_t B;
 
-    //break command flag
-    uint8_t B;
+        // unused flag
+        uint8_t U;
 
-    // unused flag
-    uint8_t U;
+        // overflow flag
+        uint8_t V;
 
-    // overflow flag
-    uint8_t V;
+        // negative flag
+        uint8_t N;
 
-    // negative flag
-    uint8_t N;
+        // interrupt type to execute
+        uint8_t interrupt;
 
-    // interrupt type to execute
-    uint8_t interrupt;
+        // number of cycles to stall
+        int stall;
 
-    // number of cycles to stall
-    int stall;
+        // core method for executing instructions
+        uint64_t Step();
 
-    // core method for executing instructions
-    uint64_t Step();
+        // interruption related methods
+        void TriggerIRQ();
+        void TriggerNMI();
+        void irq();
+        void nmi();
 
-    // interruption related methods
-    void TriggerIRQ();
-    void TriggerNMI();
-    void irq();
-    void nmi();
+        // memory push related
+        void push(uint8_t);
+        void push16(uint16_t);
 
-    // memory push related
-    void push(uint8_t);
-    void push16(uint16_t);
+        // memory pull related
+        uint8_t pull();
+        uint16_t pull16();
 
-    // memory pull related
-    uint8_t pull();
-    uint16_t pull16();
+        // memory related
+        uint16_t read16(uint16_t);
+        uint16_t read16Bug(uint16_t);
 
-    // memory related
-    uint16_t read16(uint16_t);
-    uint16_t read16Bug(uint16_t);
+        // flag related
+        void setZ(uint8_t v);
+        void setN(uint8_t v);
+        void setZN(uint8_t v);
+        void compare(uint8_t a, uint8_t b);
+        void setFlags(uint8_t flag);
+    };
 
-    // flag related
-    void setZ(uint8_t v);
-    void setN(uint8_t v);
-    void setZN(uint8_t v);
-    void compare(uint8_t a, uint8_t b);
-    void setFlags(uint8_t flag);
-};
+    // interface of instruction executor
+    typedef void (*InstructionExecutor) (Cpu* cpu, uint16_t address, bool is_accumulator);
 
-// interface of instruction executor
-typedef void (*InstructionExecutor) (NesCpu* cpu, uint16_t address);
+    // struct consisting of information for instruction execution
+    struct InstructionParams {
+        // instruction's id
+        uint8_t ID;
 
-// struct consisting of information for instruction execution
-struct InstructionParams {
-    // instruction's id
-    uint8_t ID;
+        // addressing mode
+        uint8_t AddressingMode;
 
-    // addressing mode
-    uint8_t AddressingMode;
+        // the size of each instruction in byte
+        uint8_t InstructionSizes;
 
-    // the size of each instruction in byte
-    uint8_t InstructionSizes;
+        // the number of cycles used by the instruction
+        uint8_t InstructionCycle;
 
-    // the number of cycles used by the instruction
-    uint8_t InstructionCycle;
+        // the number of page cycles used by the instruction
+        uint8_t PageCycle;
 
-    // the number of page cycles used by the instruction
-    uint8_t PageCycle;
-
-    InstructionExecutor executor;
-};
+        InstructionExecutor executor;
+    };
+}
 #endif
