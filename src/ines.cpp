9408c5e46482e54a0483e406294fe6f9d36b041f
mathetake <cz.rk.t0415y.g@gmail.com>
2018-11-06 23:43:47 +0900

    read PRG, CHR, header from .nes

--- src/ines.cpp
+++ src/ines.cpp
@@ -1,11 +1,25 @@
 
 #include <iostream>
+#include <fstream>
 #include "ines.hpp"
 
 // Ref: https://wiki.nesdev.com/w/index.php/INES
-
+// 4e45 531a 0201 0100 0000 0000 0000 0000
 namespace nestake {
-    const uint32_t iNESMagicNumber = 0x1a53454e;
+
+    // check magic number from header
+    void checkMagicNumber(const uint8_t m[4]) {
+        bool is_valid = true;
+        if ((m[0] != 0x4e) |
+            (m[1] != 0x45) |
+            (m[2] != 0x53) |
+            (m[3] != 0x1a)) {
+            is_valid = false;
+        }
+        if (!is_valid) {
+            fputs ("File error", stderr); exit (1);
+        }
+    }
 
     Cartridge::Cartridge(std::string path) {
         FILE *f = std::fopen(path.c_str(), "rb");
@@ -13,21 +27,46 @@ namespace nestake {
             fputs ("File error", stderr); exit (1);
         }
 
-        inesHeader h = inesHeader{};
-        uint8_t nesMagic[8]={};
+        // read magic number
+        uint8_t nesMagic[4]={};
+        std::fread(nesMagic, 1, 4, f);
+        checkMagicNumber(nesMagic);
 
-        std::fread(nesMagic, 1, 8, f);
+        // Size of PRG ROM in 16 KB units
+        uint8_t  numPRG;
+        std::fread(&numPRG, 1, 1, f);
+        if (numPRG == 0) {
+            fputs ("File error", stderr); exit (1);
+        }
 
-        // read header
-        std::cout << "\n\n\nh.magic: " << nesMagic[0] << "\n";
-        std::cout << "\n\n\nh.magic: " << nesMagic[1] << "\n";
-        std::cout << "\n\n\nh.magic: " << nesMagic[2] << "\n";
-        std::cout << "\n\n\nh.magic: " << nesMagic[7] << "\n";
+        // Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM)
+        uint8_t  numCHR;
+        std::fread(&numCHR, 1, 1, f);
+        if (numCHR == 0) {
+            fputs ("File error", stderr); exit (1);
+        }
 
+        // flag 6
+        uint8_t  control1;
+        std::fread(&control1, 1, 1, f);
+
+        // flag 7
+        uint8_t  control2;
+        std::fread(&control2, 1, 1, f);
+
+        // padding
+        std::fread(new(uint8_t[8]), 1, 8, f);
+
+        // read PRG
+        PRG.reserve(size_t(0x4000*numPRG));
+        for (auto i = 0; i < size_t(0x4000*numPRG); ++i ) {
+            std::fread(&PRG[i], 1, 1, f);
+        }
+
+        // read CHR
+        CHR.reserve(size_t(0x4000*numCHR));
+        for (auto i = 0; i < size_t(0x4000*numPRG); ++i ) {
+            std::fread(&CHR[i], 1, 1, f);
+        }
     }
-
-
-    // TODO: implement Save/Load
-    void Cartridge::Save() {};
-    void Cartridge::Load() {};
 }
--- src/ines.hpp
+++ src/ines.hpp
@@ -5,17 +5,6 @@
 #include <string>
 #include <vector>
 namespace nestake{
-
-    struct inesHeader {
-        uint32_t magic;
-        uint8_t  numPRG;
-        uint8_t  numCHR;
-        uint8_t  control1;
-        uint8_t  control2;
-        uint8_t  numRAM;
-        uint8_t  padding[7];
-    };
-
     class Cartridge {
     public:
         std::vector<uint8_t> PRG;
@@ -23,10 +12,7 @@ namespace nestake{
         std::vector<uint8_t> SRAM;
         uint8_t Mapper;
         uint8_t Mirror;
-        uint8_t Battery;
         explicit Cartridge(std::string);
-        void Save();
-        void Load();
     };
 
 }
